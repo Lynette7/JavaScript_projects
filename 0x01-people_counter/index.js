6ed6f8b80c62078e95a229fd5906ae071051426b
Lynette7 <lynettewanjiru7@gmail.com>
2023-03-01 11:58:49 +0300

    Functionalities

--- 0x01-people_counter/index.js
+++ 0x01-people_counter/index.js
@@ -1,12 +1,20 @@
 //document.getElementById("count-el").innerText = 5
+//textContent instead of innerText because the former includes hidden content e.g spaces & is less computationally
+//expensive
 
-
+let countEl = getElementById("count-el")
+let saveEl = getElementById("save-el")
 let count = 0
+
 function increment() {
-    count = count + 1
-    document.getElementById("count-el").innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
+    let entry = count + " - "
+    saveEl.textContent += entry
     console.log(count)
+    countEl.textContent = 0
+    count = 0
 }
